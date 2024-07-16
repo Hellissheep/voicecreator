1c05ea578b1b9a3a9f9fa79c98598d02cd0851ec
Artem <103057584+Hellissheep@users.noreply.github.com>
2024-07-16 05:45:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 settings = {
-	'TOKEN': 'OTk5Mjk3MzgxMDk3MDI5NjMy.GoCSQ7.PeDhf-aM2-LkYfd2steSMjDsrlxWg3wFG-092c',
+	'TOKEN': '******',
 	'NAME BOT': 'VCreator_bot',
-	'ID': '995677545905279106',
+	'ID': '******',
 	'PREFIX': '$',
-	'OWNER': 'DAMN#6054',
-	'OWNER NAME': 'DAMN',
-	'ADMIN ROLE 1': 'ГенСек',
-    'ADMIN ROLE 2': 'SF experience',
+	'OWNER': '******',
+	'OWNER NAME': '******',
+	'ADMIN ROLE 1': '******',
+    'ADMIN ROLE 2': '******',
 	'NEED ROLES' : '2',
-	'CHANEL ID': '998621149053321297',
-    'CATEGORY ID': '703022598782713887'
+	'CHANEL ID': '',
+    'CATEGORY ID': '******'
 }
